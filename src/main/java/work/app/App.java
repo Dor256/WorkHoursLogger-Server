594545f9f3e73946b7b017b11dc052ca9ec3f57e
Dor <dorjp6@gmail.com>
2019-09-14 15:20:31 +0300

    push heroku

--- src/main/java/work/app/App.java
+++ src/main/java/work/app/App.java
@@ -1,12 +1,14 @@
 package work.app;
 
+import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		SpringApplication.run(App.class, args);
 	}
 
