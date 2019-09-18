d82a46951387d80e9eb0d2aff65bb3b0150632aa
Dor <dorjp6@gmail.com>
2019-09-18 20:37:28 +0300

    Style changes, error handling and banner animation changes

---
+++ src/main/resources/static/precache-manifest.fccac1b38f6f8a341ec8ef1914175b82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aaac6d2520f043235263da6ef006f39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85b4aa4323feb14d98f9",
+    "url": "/static/css/main.218f70b3.chunk.css"
+  },
+  {
+    "revision": "61f2f21196916930b197",
+    "url": "/static/js/2.7edf0727.chunk.js"
+  },
+  {
+    "revision": "85b4aa4323feb14d98f9",
+    "url": "/static/js/main.91aee7ea.chunk.js"
+  },
+  {
+    "revision": "b4e7bc54e837ca20831f",
+    "url": "/static/js/runtime~main.91a3a6ad.js"
+  }
+]);
--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb29eec0aebdd07e3a9cedebeb86145f.js"
+  "/precache-manifest.fccac1b38f6f8a341ec8ef1914175b82.js"
 );
 
 self.addEventListener('message', (event) => {
