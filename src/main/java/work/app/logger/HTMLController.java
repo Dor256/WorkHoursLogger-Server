5a4f999a71b63e6e052f23cdb1cf1ce8cd5612f1
Dor <dorjp6@gmail.com>
2019-09-19 19:20:12 +0300

    cross origin

--- src/main/java/work/app/logger/HTMLController.java
+++ src/main/java/work/app/logger/HTMLController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@CrossOrigin(origins="https://work-logger-app.herokuapp.com")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 public class HTMLController {
 
--- src/main/java/work/app/logger/WorkLoggerController.java
+++ src/main/java/work/app/logger/WorkLoggerController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins="https://work-logger-app.herokuapp.com")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class WorkLoggerController {
     @Autowired
