a4aabd284fbf35eed224c168310c10c9aed42c78
Dor <dorjp6@gmail.com>
2019-09-19 19:17:15 +0300

    cross origin

--- src/main/java/work/app/logger/WorkLoggerController.java
+++ src/main/java/work/app/logger/WorkLoggerController.java
@@ -7,6 +7,7 @@ import java.text.ParseException;
 import javax.mail.MessagingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="https://work-logger-app.herokuapp.com")
 @RestController
 public class WorkLoggerController {
     @Autowired
