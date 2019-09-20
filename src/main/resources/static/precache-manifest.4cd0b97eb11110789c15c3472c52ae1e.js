0bc31599c92b8ba6590ee993e9525cb2f263e21d
Dor <dorjp6@gmail.com>
2019-09-20 16:47:41 +0300

    Add enter button indicator if user is inside, Loader, and error handling

---
+++ src/main/resources/static/precache-manifest.4cd0b97eb11110789c15c3472c52ae1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f44dd30d625618346c21de1b53fc5b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04537660e48971754fcc",
+    "url": "/static/css/main.529183f7.chunk.css"
+  },
+  {
+    "revision": "38287532accee37ba356",
+    "url": "/static/js/2.cca4d069.chunk.js"
+  },
+  {
+    "revision": "04537660e48971754fcc",
+    "url": "/static/js/main.4ec7a031.chunk.js"
+  },
+  {
+    "revision": "b4e7bc54e837ca20831f",
+    "url": "/static/js/runtime~main.91a3a6ad.js"
+  }
+]);
--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b8cafbc91487b3a71bf3fb09de7cd75d.js"
+  "/precache-manifest.4cd0b97eb11110789c15c3472c52ae1e.js"
 );
 
 self.addEventListener('message', (event) => {
