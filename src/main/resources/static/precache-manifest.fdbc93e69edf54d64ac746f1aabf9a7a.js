e5d4579a33217c41998c9989be33e42558a78817
Dor <dorjp6@gmail.com>
2019-10-30 19:05:58 +0200

    add support for admin user in client

---
+++ src/main/resources/static/precache-manifest.fdbc93e69edf54d64ac746f1aabf9a7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf1919bbbc1d303abc70f31c6f16a62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a3ab67d40da72d99f44",
+    "url": "/static/css/main.d1942600.chunk.css"
+  },
+  {
+    "revision": "cf3c560abd2c6416fb33",
+    "url": "/static/js/2.031a3bed.chunk.js"
+  },
+  {
+    "revision": "9a3ab67d40da72d99f44",
+    "url": "/static/js/main.a160c8df.chunk.js"
+  },
+  {
+    "revision": "b4e7bc54e837ca20831f",
+    "url": "/static/js/runtime~main.91a3a6ad.js"
+  }
+]);
